54d939ba3e7066cbfdf7ac55c8ab5f98314bae44
Wahyu Mentari Mayadita <140961917+whymydt@users.noreply.github.com>
2026-02-09 19:22:19 +0700

    Create Palindrome.java
    Check if inputted string can be arranged into Indonesian palindrome

---
+++ Palindrome/Palindrome.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        // Input must be in a single word, multi words will be merged into one and converted to lowercase characters
+        Scanner scan = new Scanner(System.in);
+        String inputText;
+        System.out.println("Input any word with random character order : ");
+        inputText = scan.nextLine();
+
+        // Remove whitespace and convert to lowercase
+        String textToProcess = inputText.replaceAll("\\s", "");
+        textToProcess.toLowerCase();
+
+        // Check if palindrome
+        checkPalindromeStr(textToProcess);
+    }
+
+    static int compare(String palindromeStr, String textToCompare) {
+        int[] result = new int[textToCompare.length()];
+
+        for (int i = 0; i < palindromeStr.length(); i++) {
+            for (int j = 0; j < result.length; j++) {
+                if (palindromeStr.charAt(i) == textToCompare.charAt(j)) {
+                    if(result[j] == 1){
+                        continue;
+                    } else {
+                        result[j] = 1;
+                        break;
+                    }
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        int valid = 1;
+
+        for (int k = 0; k < result.length; k++) {
+            if (result[k] == 0) {
+                valid = 0;
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        return valid;
+    }
+
+    static void checkPalindromeStr(String anyWord) {
+        // List kata palindrome
+        String[] palindromes = {
+                "isi", "kakak", "kapak", "masam",
+                "radar", "sinis", "tamat", "malam",
+                "katak", "kodok", "taat", "level",
+                "telolet", "kasurrusak", "kasurinirusak"
+        };
+
+        int[] valid = {0, 0};
+
+        // Check for exact match
+        for (int i = 0; i < palindromes.length; i++) {
+            if (palindromes[i] == anyWord) {
+                valid[0] = 1;
+                valid[1] = i;
+                break;
+            } else {
+                continue;
+            }
+        }
+
+        // Check palindrome by word
+        if (valid[0] == 0) {
+            for (int i = 0; i < palindromes.length; i++) {
+                if (anyWord.length() != palindromes[i].length()) {
+                    continue;
+                } else {
+                    if (compare(palindromes[i], anyWord) == 0) {
+                        continue;
+                    } else {
+                        valid[0] = 1;
+                        valid[1] = i;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if(valid[0] == 0){
+            System.out.printf("Huruf pembentuk teks " + anyWord + " tidak dapat membentuk palindrome!");
+        } else {
+            System.out.println("Huruf pembentuk teks " + anyWord + " dapat membentuk palindrome " + palindromes[valid[1]]);
+        }
+    }
+}
